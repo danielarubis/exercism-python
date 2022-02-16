f539f1c72a232db9e6a34adea2aa3c46f4c6ca0b
Daniela Rubis <danielarubis@hotmail.com>
2022-02-16 17:54:29 -0300

    Second exercise solution

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,4 +1,8 @@
-def two_fer(name=""):
-    if name == "":
-        return input('One for you, one for me.')
-    return input('One for ' + name + ', one for me.')
+def two_fer(name="you"):
+    return 'One for {}, one for me.'.format(name)
+
+#Option 2:
+# def two_fer(name=""):
+#     if name == "":
+#         return input('One for you, one for me.')
+#     return input('One for ' + name + ', one for me.')
